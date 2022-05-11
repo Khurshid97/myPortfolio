ec05b1f024dbfe754d9df825565d969decdca2e8
Khurshid97 <fantasticidea3000@gmail.com>
2022-05-11 17:22:09 +0200

    Corrections in static files and their directories

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mypage\static'),
 )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
